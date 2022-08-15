7b08b2e626da07f8eb546fc4aaf072dbaa01bf22
solbero <njord.solberg@gmail.com>
2022-08-15 14:34:19 +0200

    Add function to create a Deta Base query from parameters

--- ti4_mapmaker_api/database.py
+++ ti4_mapmaker_api/database.py
@@ -1,4 +1,7 @@
 import contextlib
+import itertools
+from collections.abc import Sequence
+from typing import Any, Union
 
 from deta import Deta
 
@@ -11,3 +14,11 @@ async def AsyncBase(engine: Deta, document: str):
         yield async_base
     finally:
         await async_base.close()
+
+
+def create_query(**kwargs: Union[Sequence[Any], None]) -> list[dict[str, Any]]:
+    """Creates a Data Base query object from query parameters."""
+    args = [[{key: value} for value in values] for key, values in kwargs.items() if values]
+    products = itertools.product(*args)
+    query = [{key: value for dict_ in product for key, value in dict_.items()} for product in products]
+    return query
