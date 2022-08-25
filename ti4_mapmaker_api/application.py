6f3da5e5862e267acb13077deed314d764c0e746
solbero <njord.solberg@gmail.com>
2022-08-25 15:40:54 +0200

    Rename app.py to application.py

--- ti4_mapmaker_api/application.py
+++ ti4_mapmaker_api/application.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from ti4_mapmaker_api import view
 
 
-def create_app() -> FastAPI:
-    app = FastAPI(debug=True)
+def create() -> FastAPI:
+    app = FastAPI(debug=True, docs_url="/", redoc_url=None)
     app.include_router(view.router)
     return app
