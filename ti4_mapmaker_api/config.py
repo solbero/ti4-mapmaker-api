849046444bf692165952268eefe073ed55645650
solbero <njord.solberg@gmail.com>
2022-08-25 17:57:14 +0200

    Add Deta Base document names

--- ti4_mapmaker_api/config.py
+++ ti4_mapmaker_api/config.py
@@ -1,16 +1,20 @@
 from functools import cache
 
-from pydantic import BaseSettings, Field
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """ "Class containing project settings."""
 
-    deta_project_key: str = Field(env="DETA_PROJECT_KEY")
-    deta_project_id: str = Field(env="DETA_PROJECT_ID")
+    deta_project_key: str
+    deta_project_id: str
+    deta_base_document_maps: str = "maps"
+    deta_base_document_tiles: str = "tiles"
+    deta_base_document_factions: str = "factions"
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 @cache
