b829da762218f3765e060aa4113c2ae5a1b790f8
solbero <njord.solberg@gmail.com>
2022-12-10 14:35:22 +0100

    Add database dependency injection

--- ti4_mapmaker_api/view.py
+++ ti4_mapmaker_api/view.py
@@ -1,11 +1,10 @@
+import functools
 from typing import Optional
 
 from deta import Deta
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
 
-from ti4_mapmaker_api import config
-from ti4_mapmaker_api import database as db
-from ti4_mapmaker_api import schema
+from ti4_mapmaker_api import config, database, schema
 
 settings = config.get_settings()
 project_key = settings.deta_project_key
@@ -14,14 +13,19 @@ engine = Deta(project_key)
 router = APIRouter()
 
 
+def get_detabase():
+    return functools.partial(database.AsyncBase, engine)
+
+
 @router.get("/maps/", response_model=list[schema.Map])
 async def read_maps(
     players: Optional[list[schema.Players]] = Query(None),  # noqa: B008
-    style: Optional[list[str]] = Query(None),  # noqa: B008
+    style: Optional[list[schema.Style]] = Query(None),  # noqa: B008
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    query = db.create_query(players=players, style=style)
-    document = settings.deta_base_document_maps
-    async with db.AsyncBase(engine, document) as base:
+    query = database.create_query(players=players, style=style)
+
+    async with detabase(document="maps") as base:
         results = await base.fetch(query)
     if results.count > 0:
         return [schema.Map.parse_obj(result) for result in results.items]
@@ -32,9 +36,9 @@ async def read_maps(
 @router.get("/maps/{key}", response_model=schema.Map)
 async def read_maps_by_key(
     key: str = Query(regex=r"^[1-8]{1}-[a-z]+$"),  # noqa: B008
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    document = settings.deta_base_document_maps
-    async with db.AsyncBase(engine, document) as base:
+    async with detabase(document="maps") as base:
         result = await base.get(key)
     if result:
         return schema.Map.parse_obj(result)
@@ -45,10 +49,11 @@ async def read_maps_by_key(
 @router.get("/factions/", response_model=list[schema.Faction])
 async def read_factions(
     release: Optional[list[schema.Release]] = Query(None),  # noqa: B008
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    query = db.create_query(release=release)
-    document = settings.deta_base_document_factions
-    async with db.AsyncBase(engine, document) as base:
+    query = database.create_query(release=release)
+
+    async with detabase(document="factions") as base:
         results = await base.fetch(query)
     if results.count > 0:
         return [schema.Faction.parse_obj(result) for result in results.items]
@@ -59,9 +64,9 @@ async def read_factions(
 @router.get("/factions/{key}", response_model=schema.Faction)
 async def read_factions_by_key(
     key: schema.NameSlug,
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    document = settings.deta_base_document_factions
-    async with db.AsyncBase(engine, document) as base:
+    async with detabase(document="factions") as base:
         result = await base.get(key)
     if result:
         return schema.Faction.parse_obj(result)
@@ -73,10 +78,11 @@ async def read_factions_by_key(
 async def read_tiles(
     tag: Optional[list[schema.Tag]] = Query(None),  # noqa: B008
     release: Optional[list[schema.Release]] = Query(None),  # noqa: B008
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    query = db.create_query(tag=tag, release=release)
-    document = settings.deta_base_document_tiles
-    async with db.AsyncBase(engine, document) as base:
+    query = database.create_query(tag=tag, release=release)
+
+    async with detabase(document="tiles") as base:
         results = await base.fetch(query)
     if results.count > 0:
         return [schema.Tile.parse_obj(result) for result in results.items]
@@ -87,9 +93,9 @@ async def read_tiles(
 @router.get("/tiles/{key}", response_model=schema.Tile)
 async def read_tiles_by_key(
     key: str = Query(regex=r"^\d{1,2}[A-B]?$"),  # noqa: B008
+    detabase=Depends(get_detabase),  # noqa: B008
 ):
-    document = settings.deta_base_document_tiles
-    async with db.AsyncBase(engine, document) as base:
+    async with detabase(document="tiles") as base:
         result = await base.get(key)
     if result:
         return schema.Tile.parse_obj(result)
