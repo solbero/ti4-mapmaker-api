b5bd57f3c6f44618abf66a9aef6d9689e1b06209
solbero <njord.solberg@gmail.com>
2022-12-12 13:17:44 +0100

    Add dependency injection to view.py

--- ti4_mapmaker_api/view.py
+++ ti4_mapmaker_api/view.py
@@ -1,34 +1,49 @@
 import functools
 from typing import Optional
 
-from deta import Deta
+import deta
 from fastapi import APIRouter, Depends, HTTPException, Query
 
 from ti4_mapmaker_api import config, database, schema
 
-settings = config.get_settings()
-project_key = settings.deta_project_key
-
-engine = Deta(project_key)
 router = APIRouter()
 
+project_settings = config.get_settings()
+project_key = project_settings.deta_project_key
 
-def get_detabase():
-    return functools.partial(database.AsyncBase, engine)
+deta_engine = deta.Deta(project_key)
+deta_db = functools.partial(database.AsyncBase, deta_engine)
+
+
+def get_maps_document() -> str:
+    """Returns the name of the maps document used in the database. Name is overridden during testing."""
+    return "maps"
+
+
+def get_factions_document() -> str:
+    """Returns the name of the factions document used in the database. Name is overridden during testing."""
+    return "factions"
+
+
+def get_tiles_document() -> str:
+    """Returns the name of the tiles document used in the database. Name is overridden during testing."""
+    return "tiles"
 
 
 @router.get("/maps/", response_model=list[schema.Map])
 async def read_maps(
     players: Optional[list[schema.Players]] = Query(None),  # noqa: B008
     style: Optional[list[schema.Style]] = Query(None),  # noqa: B008
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_maps_document),  # noqa: B008
 ):
+    """Endpoint for fetching all the maps or quering the maps database."""
+
     query = database.create_query(players=players, style=style)
 
-    async with detabase(document="maps") as base:
-        results = await base.fetch(query)
-    if results.count > 0:
-        return [schema.Map.parse_obj(result) for result in results.items]
+    async with deta_db(document) as db:
+        results = await db.fetch(query)
+    if len(results) > 0:
+        return [schema.Map.parse_obj(result) for result in results]
     else:
         raise HTTPException(status_code=404, detail="No entries matched query")
 
@@ -36,10 +51,12 @@ async def read_maps(
 @router.get("/maps/{key}", response_model=schema.Map)
 async def read_maps_by_key(
     key: str = Query(regex=r"^[1-8]{1}-[a-z]+$"),  # noqa: B008
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_maps_document),  # noqa: B008
 ):
-    async with detabase(document="maps") as base:
-        result = await base.get(key)
+    """Endpoint for fetching a map by key from the maps database."""
+
+    async with deta_db(document) as db:
+        result = await db.get(key)
     if result:
         return schema.Map.parse_obj(result)
     else:
@@ -49,14 +66,16 @@ async def read_maps_by_key(
 @router.get("/factions/", response_model=list[schema.Faction])
 async def read_factions(
     release: Optional[list[schema.Release]] = Query(None),  # noqa: B008
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_factions_document),  # noqa: B008
 ):
+    """Endpoint for fetching all the factions or quering the factions database."""
+
     query = database.create_query(release=release)
 
-    async with detabase(document="factions") as base:
-        results = await base.fetch(query)
-    if results.count > 0:
-        return [schema.Faction.parse_obj(result) for result in results.items]
+    async with deta_db(document) as db:
+        results = await db.fetch(query)
+    if len(results) > 0:
+        return [schema.Faction.parse_obj(result) for result in results]
     else:
         raise HTTPException(status_code=404, detail="No entries matched query")
 
@@ -64,10 +83,12 @@ async def read_factions(
 @router.get("/factions/{key}", response_model=schema.Faction)
 async def read_factions_by_key(
     key: schema.NameSlug,
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_factions_document),  # noqa: B008
 ):
-    async with detabase(document="factions") as base:
-        result = await base.get(key)
+    """Endpoint for fetching a faction by key from the factions database."""
+
+    async with deta_db(document) as db:
+        result = await db.get(key)
     if result:
         return schema.Faction.parse_obj(result)
     else:
@@ -78,14 +99,16 @@ async def read_factions_by_key(
 async def read_tiles(
     tag: Optional[list[schema.Tag]] = Query(None),  # noqa: B008
     release: Optional[list[schema.Release]] = Query(None),  # noqa: B008
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_tiles_document),  # noqa: B008
 ):
+    """Endpoint for fetching all the tiles or quering the tiles database."""
+
     query = database.create_query(tag=tag, release=release)
 
-    async with detabase(document="tiles") as base:
-        results = await base.fetch(query)
-    if results.count > 0:
-        return [schema.Tile.parse_obj(result) for result in results.items]
+    async with deta_db(document) as db:
+        results = await db.fetch(query)
+    if len(results) > 0:
+        return [schema.Tile.parse_obj(result) for result in results]
     else:
         raise HTTPException(status_code=404, detail="No entries matched query")
 
@@ -93,16 +116,13 @@ async def read_tiles(
 @router.get("/tiles/{key}", response_model=schema.Tile)
 async def read_tiles_by_key(
     key: str = Query(regex=r"^\d{1,2}[A-B]?$"),  # noqa: B008
-    detabase=Depends(get_detabase),  # noqa: B008
+    document: str = Depends(get_tiles_document),  # noqa: B008
 ):
-    async with detabase(document="tiles") as base:
-        result = await base.get(key)
+    """Endpoint for fetching a tile by key from the tiles database."""
+
+    async with deta_db(document) as db:
+        result = await db.get(key)
     if result:
         return schema.Tile.parse_obj(result)
     else:
         raise HTTPException(status_code=404, detail="No entries matched key")
-
-
-@router.get("/generate/")
-async def generate():
-    ...
